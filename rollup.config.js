d857c19b67ff9d0ffb962e39565b22e60894cc9a
luxp <luxianpo@gmail.com>
2017-07-30 20:36:36 +0800

    chore: add rollup build config

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import postcss from 'rollup-plugin-postcss'
+import cssnano from 'cssnano'
+import uglify from 'rollup-plugin-uglify'
+
+import {dependencies} from './package.json'
+const external = Object.keys(dependencies)
+
+export default {
+  entry: 'src/jqmath.js',
+  dest: 'dist/jqmath.js',
+  format: 'cjs',
+  external,
+  plugins: [
+    postcss({
+      plugins: [cssnano()],
+      extract : 'dist/jqmath.css'
+    }),
+    uglify({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ]
+}
