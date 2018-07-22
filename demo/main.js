03d946926504c738893fa712edc2257171d8cc7b
luxp <luxianpo@gmail.com>
2018-07-22 20:46:27 +0800

    update demo

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,14 @@
 import './main.css'
 import 'normalize.css'
 
-import {parseMath} from 'jqmath'
+import {parseMath, M} from 'jqmath'
 const inputElm = document.getElementById('input')
 const resultElm = document.getElementById('result')
 
 const testText = 'just write some latex in like \n $$ $ax^2+bx+c=0$ with $a≠0$'
 resultElm.innerHTML = testText
 inputElm.innerHTML = testText
+
 parseMath(resultElm)
 
 function onInput (event) {
